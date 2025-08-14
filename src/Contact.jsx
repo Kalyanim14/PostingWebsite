f7f1ee70772e84554e0a7a9e7973625a00fca5b1
Kalyani Mantramurthi <kalyanimantramurthi@gmail.com>
2025-08-14 20:00:58 +0530

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,1 +1,19 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div style={{ width: "100%", height: "600px" }}>
+      <iframe
+        frameBorder="0"
+        id="iframewin"
+        width="100%"
+        height="100%"
+        src="https://zc1.maillist-manage.in/ua/Optin?od=1a1e3dbd7a23a&zx=1dfaf9dddb&tD=13dc6f9dad690a49&sD=13dc6f9dad690e35"
+        title="Zoho Contact Form"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Contact;
 
